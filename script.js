fb0b2285e20496e1c30555e869d2964a7220e8d8
unknown <kritikapanjanani@gmail.com>
2024-01-16 23:44:55 +0530

    first commit

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ GetPuzzle.onclick = function () {
 		board = response.board
 		FillBoard(board)
 	}
-	xhrRequest.open('get', 'https://sugoku.herokuapp.com/board?difficulty=easy')
+	xhrRequest.open('get', 'https://sugoku.onrender.com/board?difficulty=easy')
 	//we can change the difficulty of the puzzle the allowed values of difficulty are easy, medium, hard and random
 	xhrRequest.send()
 }
@@ -43,6 +43,59 @@ SolvePuzzle.onclick = () => {
 	SudokuSolver(board, 0, 0, 9);
 };
 
-function SudokuSolver(board, i, j, n) {
-	// Write your Code here
+function isValid(board ,i,j, num,n){
+    //row and column check
+   for(let x=0;x<n;x++){
+       if(board[i][x]==num || board[x][j]==num){
+           return false;
+       }
+   }
+   //submatrix check
+   let rn=Math.sqrt(n);
+   let si=i-i%rn;
+   let sj=j-j%rn;
+    
+    for(let x=si;x<si;x++){
+        for(let y=sj;y<sj+rn;y++){
+            if(board[x][y]==num){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function SudokuSolver(board,
+i,j,n){
+    //Base case
+    if(i==n){
+        //Print(board,n);
+		FillBoard(board)
+        return true;
+    }
+    //if we are not inside the board
+    if(j==n){
+        return SudokuSolver(board,i+1,0,n);
+    }
+    //if cell is already filled -> just move ahead
+    if(board[i][j]!=0){
+        return SudokuSolver(board,i,j+1,n);
+    }
+    
+    //we try to fill the cell with an appropriate number
+    for(let num=1;num<=9;num++){
+        //Check if the number can be filled
+        if(isValid(board,i,j,num,n)){
+            board[i][j]=num;
+            let subAns = SudokuSolver(board,i,j+1,n);
+            if(subAns){
+                return true;
+            }
+            //Backtracking ->undo the change
+            board[i][j]=0;
+            
+        }
+    }
+    return false;
+    
 }
